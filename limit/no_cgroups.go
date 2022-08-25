3f8fc1a8112410af84fab78270c9aa4b2fc1481b
lwch <lwch748@gmail.com>
2022-08-25 16:52:14 +0800

    支持没有cgroups的系统进行软资源限制

--- limit/no_cgroups.go
+++ limit/no_cgroups.go
@@ -3,6 +3,15 @@
 
 package limit
 
+import (
+	"math"
+	"runtime"
+	"runtime/debug"
+)
+
 // Do set cgroups limit
-func (cfg *Configure) Do(agentName string) {
+func (cfg *Configure) Do(_ string) {
+	cpu := math.Ceil(float64(cfg.CPUQuota / 100))
+	runtime.GOMAXPROCS(int(cpu))
+	debug.SetMemoryLimit(int64(cfg.Memory))
 }
